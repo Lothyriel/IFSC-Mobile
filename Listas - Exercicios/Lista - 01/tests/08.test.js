9013da2e3c25a5e74a91240537f256819925d108
fastjonh <fastjonh@gmail.com>
2023-08-04 17:03:28 -0300

    lowerCase só de saca e deslocamento negativo...

--- Listas - Exercicios/Lista - 01/tests/08.test.js
+++ Listas - Exercicios/Lista - 01/tests/08.test.js
@@ -1,7 +1,8 @@
 const cifraCesar = (texto, deslocamento) => {
-    return [...texto]
+    return [...texto.toLowerCase()]
         .map(c => c.charCodeAt(0) + deslocamento % 26)
         .map(c => c > 'z'.charCodeAt(0) ? c - 26 : c)
+        .map(c => c < 'a'.charCodeAt(0) ? c + 26 : c)
         .map(c => String.fromCharCode(c))
         .join('')
 }
@@ -13,4 +14,6 @@ test('08', () => {
     expect(cifraCesar('marx', 1)).toBe('nbsy')
     expect(cifraCesar('zzzz', 1)).toBe('aaaa')
     expect(cifraCesar('aaaa', 1)).toBe('bbbb')
+    expect(cifraCesar('JOAO', 1)).toBe('kpbp')
+    expect(cifraCesar('JOAO', -1)).toBe('inzn')
 })
