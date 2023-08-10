0345b06bfdbcd5c7346b1982424cbab54cbcda6d
fastjonh <fastjonh@gmail.com>
2023-08-10 20:58:09 -0300

    added main screen with touchable to change assignments

--- Listas - Exercicios/Lista - 02/App.js
+++ Listas - Exercicios/Lista - 02/App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react'
+import { StatusBar } from 'expo-status-bar'
+import { View, TouchableHighlight, StyleSheet } from 'react-native'
+import { A1a, A1b, A1c } from './assignments/A1'
+import { A2a, A2b, A2c } from './assignments/A2'
 
 export default function App() {
+  const [screen, changeScreen] = useState(0)
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View>
+      <TouchableHighlight onPress={() => changeScreen(wraparound(screen + 1))}>
+        <AssignmentScreen screen={screen} />
+      </TouchableHighlight>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function AssignmentScreen(props) {
+  const assignment = assignments[props.screen]
+
+  if (!assignment) {
+    throw new Error("Invalid assignment screen")
+  }
+
+  return assignment()
+}
+
+function wraparound(index) {
+  return index > assignments.length - 1 ? 0 : index
+}
+
+const assignments = [
+  A1a,
+  A1b,
+  A1c,
+  A2a,
+  A2b,
+  A2c
+]
