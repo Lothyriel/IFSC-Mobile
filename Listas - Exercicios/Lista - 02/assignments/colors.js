88d28490edc87f27bb196df060e7ad26efcb8648
fastjonh <fastjonh@gmail.com>
2023-08-10 20:58:47 -0300

    adding colors file

---
+++ Listas - Exercicios/Lista - 02/assignments/colors.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+    orange: {
+        backgroundColor: 'rgb(255, 165, 0)'
+    },
+    purple: {
+        backgroundColor: 'rgb(128, 0, 128)'
+    },
+    green: {
+        backgroundColor: 'rgb(144, 238, 144)'
+    },
+    red: {
+        backgroundColor: 'rgb(240, 128, 128)',
+    },
+    blue: {
+        backgroundColor: 'rgb(173, 216, 230)',
+    },
+});
