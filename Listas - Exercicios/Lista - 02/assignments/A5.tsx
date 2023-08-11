e527d738c87bc9e840edea62282f0242683c050a
fastjonh <fastjonh@gmail.com>
2023-08-11 14:47:29 -0300

    A5

--- Listas - Exercicios/Lista - 02/assignments/A5.tsx
+++ Listas - Exercicios/Lista - 02/assignments/A5.tsx
@@ -1,10 +1,47 @@
-import { View } from 'react-native'
+import { View, StyleSheet } from 'react-native'
 import colors from './colors'
 import globalStyles from './styles'
 
 export function A5() {
     return (
-        <View>
+        <View style={globalStyles.container}>
+            <View style={[{ flex: 1 }, colors.grey]}></View>
+            <View style={[{ flex: 3 }, globalStyles.midCentered]}>
+                <View style={[styles.circle, styles.border, colors.darkBlue]}></View>
+            </View>
+            <View style={[{ flex: 5 }, colors.red]}></View>
+            <View style={[{ flex: 2 }, styles.buttons, globalStyles.row]}>
+                {RoundSquare(colors.orange)}
+                {RoundSquare(colors.purple)}
+                {RoundSquare(colors.green)}
+            </View>
         </View>
     )
 }
+
+type Color = { backgroundColor: string }
+function RoundSquare(color: Color) {
+    return <View style={[styles.border, styles.roundSquare, color]}></View>
+}
+
+const styles = StyleSheet.create({
+    border: {
+        borderWidth: 4,
+        borderColor: 'black'
+    },
+    circle: {
+        borderRadius: 100,
+        height: 145,
+        width: 145
+    },
+    roundSquare: {
+        borderRadius: 25,
+        height: 65,
+        width: 100
+    },
+    buttons: {
+        alignItems: 'center',
+        height: '100%',
+        justifyContent: 'space-evenly'
+    }
+})
