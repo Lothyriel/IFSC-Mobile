f5e38f0aae034b6b2f1026423fbaa5cef58f557e
fastjonh <fastjonh@gmail.com>
2023-08-10 22:39:29 -0300

    adding A3 to App

--- Listas - Exercicios/Lista - 02/App.tsx
+++ Listas - Exercicios/Lista - 02/App.tsx
@@ -3,6 +3,7 @@ import { StatusBar } from 'expo-status-bar'
 import { View, TouchableHighlight } from 'react-native'
 import { A1a, A1b, A1c } from './assignments/A1'
 import { A2a, A2b, A2c } from './assignments/A2'
+import { A3a, A3b, A3c } from './assignments/A3'
 
 export default function App() {
   const [screen, changeScreen] = useState(0)
@@ -38,5 +39,8 @@ const assignments = [
   A1c,
   A2a,
   A2b,
-  A2c
+  A2c,
+  A3a,
+  A3b,
+  A3c
 ]
