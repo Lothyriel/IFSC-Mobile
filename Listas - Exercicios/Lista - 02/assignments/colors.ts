162665df89b2ba612b6407fab2e967a7c38bf8e7
fastjonh <fastjonh@gmail.com>
2023-08-11 15:11:53 -0300

    adding colors

--- Listas - Exercicios/Lista - 02/assignments/colors.ts
+++ Listas - Exercicios/Lista - 02/assignments/colors.ts
@@ -13,17 +13,41 @@ const colors = StyleSheet.create({
     red: {
         backgroundColor: 'rgb(240, 128, 128)'
     },
+    lightBlue: {
+        backgroundColor: 'rgb(176, 196, 222)'
+    },
     blue: {
         backgroundColor: 'rgb(173, 216, 230)'
-    },
-    yellow: {
-        backgroundColor: 'rgb(255, 255, 0)'
     },
     darkBlue: {
         backgroundColor: 'rgb(0, 0, 255)'
     },
+    yellow: {
+        backgroundColor: 'rgb(255, 255, 0)'
+    },
     grey: {
         backgroundColor: 'rgb(128, 128, 128)'
+    },
+    darkGrey: {
+        backgroundColor: 'rgb(176, 196, 222)'
+    },
+    pink: {
+        backgroundColor: 'rgb(255, 192, 203)'
+    },
+    lightPink: {
+        backgroundColor: 'rgb(255, 228, 225)'
+    },
+    darkPink: {
+        backgroundColor: 'rgb(240, 128, 128)'
+    },
+    lightCream: {
+        backgroundColor: 'rgb(255, 239, 213)'
+    },
+    cream: {
+        backgroundColor: 'rgb(240, 128, 128)'
+    },
+    darkCream: {
+        backgroundColor: 'rgb(205, 133, 63)'
     }
 })
 
