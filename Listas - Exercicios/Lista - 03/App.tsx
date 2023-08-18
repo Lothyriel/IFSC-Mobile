6b92df38efe8a2a9a5fe1f0d5429bd8e1eca34e7
fastjonh <fastjonh@gmail.com>
2023-08-18 16:50:12 -0300

    creating project

---
+++ Listas - Exercicios/Lista - 03/App.tsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import { StatusBar } from 'expo-status-bar'
+import { View, TouchableHighlight } from 'react-native'
+import { A1 } from './assignments/A1'
+
+export default function App() {
+  const [screen, changeScreen] = useState(0)
+
+  const wraparound = (index: number) => index > assignments.length - 1 ? 0 : index
+
+  return (
+    <View>
+      <TouchableHighlight onPress={() => changeScreen(wraparound(screen + 1))}>
+        <AssignmentScreen screen={screen} />
+      </TouchableHighlight>
+      <StatusBar style='auto' />
+    </View>
+  )
+}
+
+type Props = { screen: number }
+function AssignmentScreen(props: Props) {
+  const assignment = assignments[props.screen]
+
+  if (!assignment) {
+    throw new Error('Invalid assignment screen')
+  }
+
+  return assignment()
+}
+
+const assignments = [
+  A1
+]
