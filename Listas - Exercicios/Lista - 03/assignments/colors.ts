38dd4a6ad0f68aceb470a2502d4e4eea0826dc3d
fastjonh <fastjonh@gmail.com>
2023-08-18 17:30:59 -0300

    added colors

---
+++ Listas - Exercicios/Lista - 03/assignments/colors.ts
@@ -0,0 +1,10 @@
+const colors = {
+  lightRed: 'rgb(255, 192, 203)',
+  red: 'rgb(255, 32, 32)',
+  blue: 'rgb(0, 0, 255)',
+  yellow: 'rgb(255, 255, 0)',
+  black: 'rgb(16, 12, 13)',
+  green: 'rgb(144, 238, 144)'
+}
+
+export default colors
