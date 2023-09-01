3cccee75d4fb0ce1912230560b5e6f87691e24e4
fastjonh <fastjonh@gmail.com>
2023-08-31 23:43:19 -0300

    importing assignments

--- Listas - Exercicios/Lista - 03/App.tsx
+++ Listas - Exercicios/Lista - 03/App.tsx
@@ -4,9 +4,14 @@ import { View, TouchableHighlight } from 'react-native'
 import { A1 } from './assignments/A1'
 import { A2 } from './assignments/A2'
 import { A3 } from './assignments/A3'
+import { A4 } from './assignments/A4'
+import { A5 } from './assignments/A5'
+import { A9 } from './assignments/A9'
+import { A6 } from './assignments/A6'
+import { A7 } from './assignments/A7'
 
 export default function App() {
-  const [screen, changeScreen] = useState(2)
+  const [screen, changeScreen] = useState(7)
 
   const wraparound = (index: number) => index > assignments.length - 1 ? 0 : index
 
@@ -34,5 +39,10 @@ function AssignmentScreen(props: Props) {
 const assignments = [
   A1,
   A2,
-  A3
+  A3,
+  A4,
+  A5,
+  A6,
+  A7,
+  A9
 ]
