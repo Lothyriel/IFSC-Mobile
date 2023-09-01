789276964e12229a3727d6f37e6cb1d7fba32cb0
fastjonh <fastjonh@gmail.com>
2023-09-01 02:09:13 -0300

    a9

--- Listas - Exercicios/Lista - 03/assignments/A9.tsx
+++ Listas - Exercicios/Lista - 03/assignments/A9.tsx
@@ -1,26 +1,21 @@
-import React, { useState } from 'react';
-import { View, Text, Image, StyleSheet, Switch } from 'react-native';
+import React, { useState } from 'react'
+import { View, Image, Switch } from 'react-native'
 
 export function A9() {
   const [toggled, setTogle] = useState(false)
 
   return (
-    <View style={{ height: '100%', alignItems: 'center' }}>
+    <View style={{ height: '100%', alignItems: 'center', justifyContent: 'center' }}>
       <Switch
         trackColor={{ true: 'black' }}
         thumbColor={toggled ? 'red' : 'white'}
         onValueChange={() => setTogle(!toggled)}
-        value={toggled}
       />
-      <Image style={{ width: '50%', resizeMode: 'contain' }} source={getImage(toggled)}></Image>
+      <Image style={{ width: '50%', height: '30%', resizeMode: 'contain' }} source={getImage(toggled)}></Image>
     </View >
   )
 }
 
 function getImage(toggled: boolean) {
-  return toggled ? require('../assets/pokebola-aberta.png') : require('../assets/pokebola-fechada.png');
+  return toggled ? require('../assets/pokebola-aberta.png') : require('../assets/pokebola-fechada.png')
 }
-
-const styles = StyleSheet.create({
-
-});
