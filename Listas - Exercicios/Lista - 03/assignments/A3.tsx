457184378fe1e93ffb2c41d4363e85168f5a6e0f
fastjonh <fastjonh@gmail.com>
2023-08-31 23:44:18 -0300

    a3

--- Listas - Exercicios/Lista - 03/assignments/A3.tsx
+++ Listas - Exercicios/Lista - 03/assignments/A3.tsx
@@ -8,11 +8,11 @@ export function A3() {
         Esse é mais um exemplo de texto.
       </Text>
       <View style={{ flex: 2, flexDirection: 'row' }}>
-        <Text style={styles.black}>
+        <Text style={[styles.black, styles.rightAligned]}>
           Porém aqui vamos combinar style e flexbox para dividir o texto em duas colunas.
           Essa é a primeira coluna! Aqui vai um exemplo de texto qualquer.
         </Text>
-        <Text style={styles.yellow}>
+        <Text style={[styles.yellow, styles.rightAligned]}>
           Esse é outro exemplo de texto para popular a segunda coluna,
           porém sem criatividade para escrever mais! :D
         </Text>
@@ -30,16 +30,19 @@ const styles = StyleSheet.create({
     backgroundColor: colors.green,
   },
   black: {
-    flex: 1,
-    fontSize: 21,
+    fontSize: 20,
     backgroundColor: colors.black,
     color: colors.white
   },
   yellow: {
-    flex: 1,
-    fontSize: 26,
+    fontSize: 24,
     backgroundColor: colors.yellow,
     color: colors.blue,
-    textAlign: 'left'
+  },
+  rightAligned: {
+    paddingHorizontal: '5%',
+    textAlign: 'left',
+    textAlignVertical: 'center',
+    flex: 1,
   }
 })
